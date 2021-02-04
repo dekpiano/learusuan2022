dd324fe7a96a06ac79bd660cc195485d8b8e8cab
dekpiano <dekpiano4815@gmail.con>
2021-02-04 15:45:08 +0700

    04/04/2564 15:44 อัพเดพแก้ไขข้อมูลของนักเรียน

--- application/views/students/StudentsHome.php
+++ application/views/students/StudentsHome.php
@@ -1,27 +1,34 @@
-<div class="page-content d-flex align-items-stretch"> 
-        <!-- Side Navbar -->
-        <?php $this->load->view('students/layout/menu_left_students.php');?>
+<div class="page-content d-flex align-items-stretch">
+    <!-- Side Navbar -->
+    <?php $this->load->view('students/layout/menu_left_students.php');?>
 
-        
-        <div class="content-inner">
-          <!-- Page Header-->
-          <header class="page-header">
+
+    <div class="content-inner">
+        <!-- Page Header-->
+        <header class="page-header">
             <div class="container-fluid">
-              <h2 class="no-margin-bottom">ประชาสัมพันธ์</h2>
+                <h2 class="no-margin-bottom">ประชาสัมพันธ์</h2>
             </div>
-          </header>
-          <!-- Dashboard Counts Section-->
-          <section class="dashboard-counts no-padding-bottom">
+        </header>
+        <!-- Dashboard Counts Section-->
+        <section class="dashboard-counts no-padding-bottom">
             <div class="container-fluid">
-              <div class="row bg-white has-shadow">
-         
+                <div class="row bg-white has-shadow border border-primary mb-2">
+                    <i class="fas fa-bullhorn fa-1x"></i>
+                    <span class="pl-2 pr-2">ขณะนี้สามารถแก้ไขข้อมูลให้ระบบได้ </span>
+                    <a href="<?=base_url('StudentsEdit');?>" class="btn btn-primary btn-sm">คลิกที่นี่</a>
+                </div>
 
+                <div class="row bg-white has-shadow border border-primary mb-2">
+                    <i class="fas fa-bullhorn fa-1x"></i>
+                    <span class="pl-2 pr-2">ขณะนี้ระบบรับสมัครนักเรียนเฉพาะชั้นมัธยมศึกษาปีที่ 3 จากโรงเรียนเดิม
+                        ที่จะศึกษาต่อชั้นมัธยมศึกษาปีที่ 4 ถึง 7 กุมภาพันธ์ 2564
+                    </span>
+                </div>
 
-
-              </div>
             </div>
-          </section>
- 
-  
-        </div>
-      </div>
+        </section>
+
+
+    </div>
+</div>
--- application/views/students/layout/menu_top_students.php
+++ application/views/students/layout/menu_top_students.php
@@ -11,7 +11,7 @@
             <div class="navbar-holder d-flex align-items-center justify-content-between">
               <!-- Navbar Header-->
               <div class="navbar-header">
-                <!-- Navbar Brand --><a href="index.html" class="navbar-brand d-none d-sm-inline-block">
+                <!-- Navbar Brand --><a href="<?=base_url('StudentHome')?>" class="navbar-brand d-none d-sm-inline-block">
                   <div class="brand-text d-none d-lg-inline-block"><span>ระบบ </span><strong>รับสมัครนักเรียน (สำหรับนักเรียน) </strong></div>
                   <div class="brand-text d-none d-sm-inline-block d-lg-none"><strong>RS SKJ</strong></div></a>
                 <!-- Toggle Button--><a id="toggle-btn" href="#" class="menu-btn active"><span></span><span></span><span></span></a>
